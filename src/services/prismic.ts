7fb5ef6e8837d84324c61415f718a1843df25e5f
Guilherme Naboa <guilhermenaboa31@gmail.com>
2021-10-21 14:55:24 -0300

    Update prismic.ts

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(https://blognaboa.cdn.prismic.io/api/v2, {
+  const prismic = Prismic.client('https://blognaboa.cdn.prismic.io/api/v2', {
     req,
       
   });
